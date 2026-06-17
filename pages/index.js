1975d9a3f25a0505bba5b0128a890464791e2d25
max antony janccokallo <izumi>
2026-06-17 16:29:04 -0500

    nuevo

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,11 @@ export default function Home() {
       <Head>
         <title>Inicio | Mi Sitio SEO</title>
         <meta name="description" content="Página optimizada SEO en Next.js" />
-
-        <meta property="og:title" content="Inicio | Mi Sitio SEO" />
-        <meta property="og:description" content="Aprende SEO con Next.js" />
-        <meta property="og:type" content="website" />
       </Head>
 
-      <h1>Home</h1>
+      <main>
+        <h1>Bienvenido a mi página optimizada</h1>
+      </main>
     </>
   );
 }
