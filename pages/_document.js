aa5f23973d0aa35f258627abb4ea8d679d1ce6f8
max antony janccokallo <izumi>
2026-06-17 15:38:13 -0500

    deploy render

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="google-site-verification" content="K3VzuKz38x29A8eh5nBP2UBdU4UvIvT9t_lS0eRaaaA" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ pages/api/sitemap.js
@@ -0,0 +1,15 @@
+const BASE_URL = "https://semana-14-rosy.vercel.app";
+
+export default function handler(req, res) {
+  const urls = ["/", "/blog", "/contacto"];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`)
+  .join("")}
+</urlset>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.status(200).send(sitemap);
+}
